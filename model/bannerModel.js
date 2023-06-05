874cb5d33057f5bc0fbc22e2da3badb091429fab
Sushant <sushantonly961@gmail.com>
2023-06-05 12:26:58 +0530

    banner model added

---
+++ model/bannerModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const bannerSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Banner = mongoose.model("Banner", bannerSchema);
+
+module.exports = Banner;
