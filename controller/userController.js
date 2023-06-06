8cadb09d554d1c9cbda31dfa1d91de9e2c8c7b23
Sushant <sushantonly961@gmail.com>
2023-06-06 18:41:43 +0530

    all admin and delete admin are done

--- controller/userController.js
+++ controller/userController.js
@@ -1,23 +1,56 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/userModel");
 
-const register = async (req, res) => {
-  const { username, password } = req.body;
-
+const allAdmin = async (req, res) => {
   try {
+    const users = await User.find();
+    res.render("admin/allAdmin", { users, title: "selectMyCollege" });
+  } catch (error) {
+    // req.flash('message', 'Failed to fetch users');
+    res.redirect("/login");
+  }
+};
+
+const registerView = async (req, res) => {
+  res.render("register", { title: "selectMyCollege" });
+};
+
+const register = async (req, res) => {
+  const { username, password, adminRole } = req.body;
+  try {
+    // Check if username already exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: "Username already exists" });
+      // req.flash("message", "Username already exists");
+      return res.redirect("/register");
     }
+
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({
+
+    // Set the default role
+    let role = "user";
+
+    // Check if the adminRole checkbox is checked
+    if (adminRole) {
+      role = "admin";
+    }
+
+    // Create a new user
+    const newUser = new User({
       username,
       password: hashedPassword,
+      role,
     });
 
-    res.status(201).json({ message: "User registered successfully" });
+    // Save the user to the database
+    await newUser.save();
+
+    // req.flash("message", "User registered successfully");
+    res.redirect("/login");
   } catch (error) {
-    res.status(500).json({ message: "Registration failed" });
+    // req.flash("message", "Registration failed");
+    res.redirect("/register");
   }
 };
 
@@ -49,9 +82,29 @@ const login = async (req, res) => {
   }
 };
 
+const deleteAdmin = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await User.findByIdAndDelete(id);
+    // req.flash("message", "User deleted successfully");
+    res.redirect("/allAdmin");
+  } catch (error) {
+    // req.flash("message", "Failed to delete user");
+    res.redirect("/allAdmin");
+  }
+};
+
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect("/");
 };
 
-module.exports = { register, login, logout, loginView };
+module.exports = {
+  register,
+  login,
+  logout,
+  loginView,
+  registerView,
+  allAdmin,
+  deleteAdmin,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../controller/userController");
 
 // Register a new user
+router.get("/register", controller.registerView);
 router.post("/register", controller.register);
 
 // Login a user
@@ -11,5 +12,9 @@ router.post("/login", controller.login);
 
 // Logout a user
 router.get("/logout", controller.logout);
+
+router.get("/allAdmin", controller.allAdmin);
+
+router.delete("/admin/:id", controller.deleteAdmin);
 
 module.exports = router;
